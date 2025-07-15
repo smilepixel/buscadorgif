7945c0f5c11fdbc6f97335fbfb321b7844c661ca
smilepixel <wesleyxgm@gmail.com>
2025-07-14 18:10:36 -0600

    second

--- src/api.js
+++ src/api.js
@@ -1,10 +1,6 @@
-export const api = async () => {
+export const api = async (limit=3 ,busqueda="emoji") => {
 
     const key = "qiNWsEiO1MxNo9kscZYsHZ6XcEuo11k0"
-
-    const limit = 3
-
-    const busqueda = "emoji"
 
 const url = `https://api.giphy.com/v1/stickers/search?api_key=${key}&q=${busqueda}&limit=${limit}&offset=0&rating=g&lang=en&bundle=messaging_non_clips;`
 
